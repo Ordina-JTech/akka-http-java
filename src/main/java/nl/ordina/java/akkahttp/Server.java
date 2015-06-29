4ff5cd994a7ada53fb9b4387a3655322c700a88c
Martijn Blankestijn <martijnblankestijn@gmail.com>
2015-06-29 16:41:30 +0200

    Formatting

--- src/main/java/nl/ordina/java/akkahttp/Server.java
+++ src/main/java/nl/ordina/java/akkahttp/Server.java
@@ -18,76 +18,75 @@ import static akka.http.javadsl.marshallers.jackson.Jackson.jsonAs;
 import static akka.http.javadsl.model.HttpResponse.create;
 import static akka.http.javadsl.server.RequestVals.entityAs;
 import static akka.http.javadsl.server.values.PathMatchers.uuid;
-import static akka.http.scaladsl.model.StatusCodes.Created;
-import static akka.http.scaladsl.model.StatusCodes.InternalServerError;
+import static akka.http.scaladsl.model.StatusCodes.*;
 
 public class Server extends HttpApp {
-    private GroupRepo groups = new GroupRepo();
+  private GroupRepo groups = new GroupRepo();
 
-    @Override
-    public Route createRoute() {
+  @Override
+  public Route createRoute() {
 
         /* Dezelfde instantie moet gebruikt worden voor het opvragen van een PathMatcher! */
-        PathMatcher<UUID> uuidExtractor = uuid();
+    PathMatcher<UUID> uuidExtractor = uuid();
 
-        return handleExceptions(e -> {
-                    e.printStackTrace();
-                    return complete(create().withStatus(InternalServerError()));
-                },
-                pathSingleSlash().route(
-                        getFromResource("web/index.html")
-                ),
-                pathPrefix("groups").route(
-                        get(pathEndOrSingleSlash().route(
-                                handleWith(ctx -> ctx.completeAs(Jackson.json(), groups.getAll()))
-                        )),
-                        get(path(uuidExtractor).route(
-                                handleWith(uuidExtractor,
-                                        (ctx, uuid) -> ctx.completeAs(Jackson.json(), groups.get(uuid))
-                                )
-                        )),
-                        post(
-                                handleWith(entityAs(jsonAs(Group.class)),
-                                        (ctx, group) -> {
-                                            Group saved = groups.create(group);
-                                            return ctx.complete(HttpResponse.create()
-                                                    .withStatus(Created())
-                                                    .addHeader(
-                                                            Location.create(
-                                                                    Uri.create("http://localhost:8080/groups/" + saved.getUuid()))));
-                                        })
-                        ),
-                        put(path(uuidExtractor).route(
-                                handleWith(uuidExtractor, entityAs(jsonAs(Group.class)),
-                                        (ctx, uuid, group) -> {
-                                            if (!Objects.equals(group.getUuid(), uuid))
-                                                return ctx.completeWithStatus(StatusCodes.BadRequest());
-                                            else {
-                                                groups.update(group);
-                                                return ctx.completeWithStatus(StatusCodes.OK());
-                                            }
-                                        }
-                                )
-                        )),
-                        put(path(uuidExtractor).route(
-                                        handleWith(uuidExtractor,
-                                                (ctx, uuid) -> {
-                                                    groups.delete(uuid);
-                                                    return ctx.completeWithStatus(StatusCodes.OK());
-                                                }
-                                        )
-                        ))
-
+    return handleExceptions(e -> {
+          e.printStackTrace();
+          return complete(create().withStatus(InternalServerError()));
+        },
+        pathSingleSlash().route(
+            getFromResource("web/index.html")
+        ),
+        pathPrefix("groups").route(
+            get(pathEndOrSingleSlash().route(
+                handleWith(ctx -> ctx.completeAs(Jackson.json(), groups.getAll()))
+            )),
+            get(path(uuidExtractor).route(
+                handleWith(uuidExtractor,
+                    (ctx, uuid) -> ctx.completeAs(Jackson.json(), groups.get(uuid))
                 )
-        );
-    }
+            )),
+            post(
+                handleWith(entityAs(jsonAs(Group.class)),
+                    (ctx, group) -> {
+                      Group saved = groups.create(group);
+                      return ctx.complete(HttpResponse.create()
+                          .withStatus(Created())
+                          .addHeader(
+                              Location.create(
+                                  Uri.create("http://localhost:8080/groups/" + saved.getUuid()))));
+                    }
+                )
+            ),
+            put(path(uuidExtractor).route(
+                handleWith(uuidExtractor, entityAs(jsonAs(Group.class)),
+                    (ctx, uuid, group) -> {
+                      if (!Objects.equals(group.getUuid(), uuid))
+                        return ctx.completeWithStatus(BadRequest());
+                      else {
+                        groups.update(group);
+                        return ctx.completeWithStatus(OK());
+                      }
+                    }
+                )
+            )),
+            put(path(uuidExtractor).route(
+                handleWith(uuidExtractor,
+                    (ctx, uuid) -> {
+                      groups.delete(uuid);
+                      return ctx.completeWithStatus(OK());
+                    }
+                )
+            ))
+        )
+    );
+  }
 
-    public static void main(String[] args) throws IOException {
-        ActorSystem akkaSystem = ActorSystem.create("akka-http-example");
-        new Server().bindRoute("localhost", 8080, akkaSystem);
+  public static void main(String[] args) throws IOException {
+    ActorSystem akkaSystem = ActorSystem.create("akka-http-example");
+    new Server().bindRoute("localhost", 8080, akkaSystem);
 
-        System.out.println("<ENTER> to exit!");
-        System.in.read();
-        akkaSystem.shutdown();
-    }
+    System.out.println("<ENTER> to exit!");
+    System.in.read();
+    akkaSystem.shutdown();
+  }
 }
